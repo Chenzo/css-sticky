fbb989fc6af432d7314eb0612416abc3cc4bcfce
Vince Suriani <chenzorama@hotmail.com>
2018-02-28 15:00:41 -0500

    added flexy

---
+++ www/flex-test.php
@@ -0,0 +1,58 @@
+<?php 
+include "includes/globals.php"; 
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Sticky test</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="/css/styles.css?v=?v=<?=$cacheBusterNumber; ?>" />
+    </head>
+    <body class="flexpage">
+
+    <div class="page_margins">
+
+        <section class="halfie">
+            <div class="theimage one"></div>
+            <div class="thetext">
+            Poop in a handbag look delicious and drink the soapy mopping up water then puke giant foamy fur-balls you call this cat food, going to catch the red dot today going to catch the red dot today. Throw down all the stuff in the kitchen missing until dinner time you have cat to be kitten me right meow. Make meme, make cute face have a lot of grump in yourself because you can't forget to be grumpy and not be like king grumpy cat flex claws on the human's belly and purr like a lawnmower or suddenly go on wild-eyed crazy rampage or stare at the wall, play with food and get confused by dust sniff sniff. Claws in your leg. I cry and cry and cry unless you pet me, and then maybe i cry just for fun chase red laser dot and sun bathe, meowing chowing and wowing licks paws for spit up on light gray carpet instead of adjacent linoleum hiss at vacuum cleaner. Cat cat moo moo lick ears lick paws eat from dog's food or licks your face, stare at ceiling light pose purrfectly to show my beauty stretch. Cat snacks i show my fluffy belly but it's a trap! if you pet it i will tear up your hand asdflkjaertvlkjasntvkjn (sits on keyboard). Spread kitty litter all over house poop on grasses, try to jump onto window and fall while scratching at wall yet go into a room to decide you didn't want to be in there anyway howl on top of tall thing cat slap dog in face try to hold own back foot to clean it but foot reflexively kicks you in face, go into a rage and bite own foot, hard. Have my breakfast spaghetti yarn russian blue so scratch me there, elevator butt brown cats with pink ears for cats go for world domination. Cough hairball on conveniently placed pants burrow under covers, or cat slap dog in face but cat fur is the new black . Dream about hunting birds reward the chosen human with a slow blink, yet fall over dead (not really but gets sypathy) hate dog lay on arms while you're using the keyboard but rub face on everything, so somehow manage to catch a bird but have no idea what to do next, so play with it until it dies of shock. 
+            </div>
+        </section>
+
+        <section class="halfie">
+            <div class="theimage alt"></div>
+            <div class="thetext">
+            Thug cat . Lick butt and make a weird face lick arm hair scream at teh bath hopped up on catnip, and loves cheeseburgers with tail in the air but roll over and sun my belly. Meow to be let out curl into a furry donut i like big cats and i can not lie hide when guests come over, or brown cats with pink ears i'm going to lap some water out of my master's cup meow cat not kitten around . 
+            </div>
+        </section>
+
+        <section class="halfie">
+            <div class="theimage three"></div>
+            <div class="thetext">
+                <div>
+                    <p>Litter kitter kitty litty little kitten big roar roar feed me annoy kitten brother with poking so eat owner's food. Furrier and even more furrier hairball russian blue paw at beetle and eat it before it gets away the door is opening! how exciting oh, it's you, meh intrigued by the shower. Destroy couch put butt in owner's face. </p>
+                    <p>Damn that dog . Cat cat moo moo lick ears lick paws. Cats go for world domination annoy kitten brother with poking, sleep nap for massacre a bird in the living room and then look like the cutest and most innocent animal on the planet paw at your fat belly yet destroy couch. Sleep nap annoy owner until he gives you food say meow repeatedly until belly rubs, feels good always hungry or bleghbleghvomit my furball really tie the room together yet chase ball of string and my water bowl is clean and freshly replenished, so i'll drink from the toilet for lick the plastic bag. Yowling nonstop the whole night chill on the couch table for meow meow. Damn that dog scoot butt on the rug for sleep on keyboard, yet spend all night ensuring people don't sleep sleep all day. Demand to have some of whatever the human is cooking, then sniff the offering and walk away chase the pig around the house rub face on owner burrow under covers. Present belly, scratch hand when stroked roll over and sun my belly. I like cats because they are fat and fluffy jump around on couch, meow constantly until given food, . </p>    
+                </div>
+            </div>
+        </section>
+
+        <section class="halfie">
+            <div class="theimage four"></div>
+            <div class="thetext">
+            Nap all day scratch or sniff sniff or annoy the old grumpy cat, start a fight and then retreat to wash when i lose so kitten is playing with dead mouse cat cat moo moo lick ears lick paws. I like cats because they are fat and fluffy. Cereal boxes make for five star accommodation when in doubt, wash cats are fats i like to pets them they like to meow back and i just saw other cats inside the house and nobody ask me before using my litter box i can haz. A nice warm laptop for me to sit on fall over dead (not really but gets sypathy). Climb leg. 
+            </div>
+        </section>
+
+
+    </div>
+
+    <?php include "includes/foot_include.php"; ?>
+
+    </body>
+</html>
